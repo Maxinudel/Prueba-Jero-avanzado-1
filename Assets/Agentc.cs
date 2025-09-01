b68997052448eb96232023689b27c85c37f5553a
Maxinudel <166538999+Maxinudel@users.noreply.github.com>
2025-09-01 10:32:55 -0300

    Initial commit

---
+++ Assets/Agentc.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agentc : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    [SerializeField] NavMeshAgent agent1;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        agent1 = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent1.SetDestination(target.position);
+    }
+}
---
+++ Assets/Animcontrol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Animcontrol : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] NavMeshAgent navMeshAgent;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Speed", navMeshAgent.velocity.magnitude);
+    }
+}
